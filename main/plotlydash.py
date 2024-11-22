101809b5f9160414da4d75d30ef51f0747a5ed74
pythontutorials100 <systems.arw@gmail.com>
2024-11-22 11:44:17 -0800

    Create plotlydash.py

---
+++ main/plotlydash.py
@@ -0,0 +1,30 @@
+Overall, took 3-pronged approach: 1. Establish Strong Theory – 2. Ensure Practical Implementation – 3. Help Generate Business Value for Customers 
+
+•	Established strong theoretical concepts of ontology in projects and converted those into tangible business value for our customers by establishing a practical Knowledge Graph implementation framework. Spearheaded the development of knowledge graphs based on formal ontologies, advancing data utilization. Established a process to transfer ontologies from editing tools (Protégé) to knowledge graph tools without data loss, ensuring integrity. Developed know of how of querying kw graphs, implementing inference, defining reasoning rules and conducting analysis of results. 
+
+•	Took ownership of new REO, DPHM, and MRO efforts, leading them to successful execution. Analyzed workshop DSQs to develop and define ontology classes aligned with REO standards. 
+o	Devised a way to modify existing enterprise ontologies without disrupting IRIs, enhancing system stability. Developed mechanisms to merge ontologies effectively, improving integration processes. Coordinated with the Ontology Governance Group to ensure seamless ontology publishing and integration. 
+o	Baselined and systematized classes, object properties, data properties, axioms, and logic in REO, MRO, and PDHM. Created comparison tools to present ontology content in an accessible manner, improving audience understanding and distribution.
+
+•	Stakeholder Engagement on Collins OEE Ontology: Collaborated with stakeholders to develop ontologies, verify consistency, integrate data, and create knowledge graphs.
+•	Developed queries, graphs, and analysis charts; transferred these tools to customers, which helped in faster modeling and implementation on their side. 
+•	Educated customers on ontology and knowledge graph development, 
+•	Got recognized efforts.
+•	For P&WC DPHM Project, developed ontologies based on DSQs, gathered and mapped real-world data on ontologies to query and analyze kw graphs. Maintained effective customer interactions.
+•	For P&WA F135 Ontology Project, worked on DSQ development, studied use cases, and provided expert insights to progress the project.
+•	Addressed formal logic and reasoning mechanism questions from P&W, delivering comprehensive answers and facilitating understanding about REO and its capabilities.
+•	Created narratives on knowledge graph implementation, reasoning, and data integration to encourage adoption.
+•	Made Contribution to Organizational Knowledge. Developed and delivered OHub articles on concept relationships and reasoning, enhancing organizational outreach. Established a process to transfer ontologies from editing tools (Protégé) to knowledge graph tools without data loss, ensuring integrity.
+•	Mapped the Information Labeling and Handling Ontology to REO. 
+•	Assessed and reported the Flurry knowledge graph tool.
+•	Regarding supporting the RMD System-of-Systems ontology workshop, Contributed to the aftermarket DSQ workshop, aiding in early decision-making on scope and business value determination work.
+•	Generative Manufacturing IRAD Demo Delivery: Developed ontology, populated graphs with real data, created queries and reasoning rules, and built a user interface for the generative manufacturing demo. Communicated the overall project details via report. 
+
+                                                                                                                                                                   
+•	Supported execution through optimized resource utilization: Ensured alignment of work performed with allocated budgets and actual spending.
+•	Maintained Project Accountability. Kept accurate timekeeping within the assigned scope.
+•	Enhanced Stakeholder Engagement. Supported reviews, demos, and presentations by preparing and delivering required materials.
+
+----------
+•	Created materials for REO legal operations to aid stakeholders unfamiliar with ontology and knowledge graphs. 
+•	Supported presentation preparations for Boeing, Airbus, and OMG engagements, enhancing organizational representation.
